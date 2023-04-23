79608e2c6c460523089f58b3a03f1135f40c6f48
Daryl <darylgialolo@gmail.com>
2023-04-23 12:24:01 +0800

    Initial Commit

---
+++ Button Effects/Button_EffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Button_EffectsApp.swift
+//  Button Effects
+//
+//  Created by Wind Versi on 23/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Button_EffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Button Effects/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Button Effects
+//
+//  Created by Wind Versi on 23/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
